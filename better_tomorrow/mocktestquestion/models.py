2ecf5370dbcec4ddd42eaf4290af426da54725c2
maheh <maheshmb7712@gmail.com>
2022-12-30 23:16:54 +0530

    configued apicall for mocktest

--- better_tomorrow/mocktestquestion/models.py
+++ better_tomorrow/mocktestquestion/models.py
@@ -4,6 +4,6 @@ from mocktest.models import Mocktest
 # Create your models here.
 class MockTestQuestion(models.Model):
     id=models.IntegerField(primary_key=True)
-    # subject_id=models.ForeignKey(Subject,on_delete=models.CASCADE)
+    subject_id=models.ForeignKey(Subject,on_delete=models.CASCADE)
     mock_test_id=models.ForeignKey(Mocktest,on_delete=models.CASCADE)
     question=models.TextField(max_length=500)
