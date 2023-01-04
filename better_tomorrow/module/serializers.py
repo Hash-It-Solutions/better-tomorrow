91ec7f3d06cf41c28dcd231f6a5c99500f357595
maheh <maheshmb7712@gmail.com>
2023-01-04 23:42:00 +0530

    mocktest in progress

--- better_tomorrow/module/serializers.py
+++ better_tomorrow/module/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 from .models import Module
 class ModuleSerializer(serializers.ModelSerializer):
+    course=serializers.RelatedField(source='Module',read_only=True)
     class Meta:
         model=Module
+    # def to_representation(self, instance):
+    #     representation = dict()
+    #     representation["id"] = instance.id
+    #     representation["course"] = instance.course
+    #     representation["title"] = instance.title
+    #     representation["description"] = instance.description
+    #     return representation
+
+        fields=('id','title','description','course')
         fields='__all__'
