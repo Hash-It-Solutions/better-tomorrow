5186dbf9ef204b27083dcecc1ea85ae6179da13e
maheh <maheshmb7712@gmail.com>
2023-01-07 09:25:52 +0530

    login functionalities of custom admin is completed

--- better_tomorrow/custom_admin/urls.py
+++ better_tomorrow/custom_admin/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from custom_admin import views
 urlpatterns = [
-   path('customadmin/',views.Custom_Admin,name='Custom_Admin'),
+   path('customadmin/',views.Custom_Admin_Login,name='Custom_Admin'),
+   path('customadmin_dashboard/',views.Custom_Admin_Dashboard,name='Custom_Admin_Dashboard'),
 ]
--- better_tomorrow/custom_admin/views.py
+++ better_tomorrow/custom_admin/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,HttpResponseRedirect,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 # Create your views here.
-def Custom_Admin(request):
-    return render(request,'CustomAdmin/index.html')
+
+def Custom_Admin_Login(request):  
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            return redirect('Custom_Admin_Dashboard')
+        else:
+            return HttpResponse('username or password is not correct')
+    return render(request,'CustomAdmin/login.html')
+
+     
+
+def Custom_Admin_Dashboard(request):
+     return render(request,'CustomAdmin/index.html')
+    
+
+
+# def Custom_Admin_Login(request):     
+#         if request.method == 'POST':
+#             username = request.POST.get('username')
+#             password = request.POST.get('password')
+#             user=User.objects.filter(username=username)
+#             if not user.exists():
+#                  messages.info(request,'Account Not Found')
+#                  return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+#             user = authenticate(request, username=username, password=password)
+#             if user and user.is_superuser:
+#                 login(request,user)
+#                 return redirect('Custom_Admin_Dashboard')
+#             else:
+#                 messages.info(request,'Invalid Password')
+#                 return redirect('/')
+#         return render(request, 'CustomAdmin/login.html')
