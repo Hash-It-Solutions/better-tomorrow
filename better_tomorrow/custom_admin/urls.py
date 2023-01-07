6c42c2dad32ec22078d143681e9a553e3ddbb475
maheh <maheshmb7712@gmail.com>
2023-01-07 20:32:05 +0530

    admin side of adding course in progress

--- better_tomorrow/custom_admin/urls.py
+++ better_tomorrow/custom_admin/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from custom_admin import views
 urlpatterns = [
    path('customadmin/',views.Custom_Admin_Login,name='Custom_Admin'),
-   path('customadmin_dashboard/',views.Custom_Admin_Dashboard,name='Custom_Admin_Dashboard'),
+   path('customadmin_home/',views.Custom_Admin_Home,name='Custom_Admin_Home'),
+   path('customadmin_dashbord/',views.Custom_Admin_Dashboard,name='Custom_Admin_Dashboard'),
+   path('Course_admin_add/',views.Course_admin_add,name='Course_admin_add'),
+   path('Course_admin_View/',views.Course_admin_View,name='Course_admin_View'),
 ]
--- better_tomorrow/custom_admin/views.py
+++ better_tomorrow/custom_admin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,HttpResponseRedirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from course.models import Course
 # Create your views here.
 
 def Custom_Admin_Login(request):  
@@ -11,17 +12,35 @@ def Custom_Admin_Login(request):
         user = authenticate(request, username=username, password=password)
         if user != None:
             login(request, user)
-            return redirect('Custom_Admin_Dashboard')
+            return redirect('Custom_Admin_Home')
         else:
             return HttpResponse('username or password is not correct')
     return render(request,'CustomAdmin/login.html')
 
      
 
-def Custom_Admin_Dashboard(request):
+def Custom_Admin_Home(request):
      return render(request,'CustomAdmin/index.html')
+
+
+def Custom_Admin_Dashboard(request):
+    return render(request,'CustomAdmin/dashboard.html')
     
 
+def Course_admin_add(request):
+    if request.method == 'POST':
+        id = request.POST.get('course_id')
+        name = request.POST.get('course_name')
+        description = request.POST.get('course_description')
+        obj=Course(id=id,name=name,description=description)
+        obj.save()
+        return HttpResponse('course added successfully')
+    return render(request,'CustomAdmin/Add_Courses.html')
+
+
+def Course_admin_View(request):
+    objs=Course.objects.all()
+    return render(request,'CustomAdmin/View_Courses.html',{'objs':objs})
 
 # def Custom_Admin_Login(request):     
 #         if request.method == 'POST':
